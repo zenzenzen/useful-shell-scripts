327540760d92af0dc368d6aeb180c8ea3e75897d
johnsondeng <johnson.jy.deng@gmail.com>
2024-02-10 06:24:45 +0800

    First commit: copied over an existing utility shell I wrote at work for fun

---
+++ git_utilities.sh
@@ -0,0 +1,241 @@
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⢀⣴⠟⠉⠀⠀⠀⠈⠻⣦⡀⠀⠀⠀⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣀⢀⣾⠿⠻⢶⣄⠀⠀⣠⣶⡿⠶⣄⣠⣾⣿⠗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢻⣿⣿⡿⣿⠿⣿⡿⢼⣿⣿⡿⣿⣎⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠉⠛⢛⣛⡉⠀⠀⠙⠛⠻⠛⠑⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣧⣤⣴⠿⠿⣷⣤⡤⠴⠖⠳⣄⣀⣹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣀⣟⠻⢦⣀⡀⠀⠀⠀⠀⣀⡈⠻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡿⠉⡇⠀⠀⠛⠛⠛⠋⠉⠉⠀⠀⠀⠹⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡟⠀⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠈⠑⠪⠷⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣦⣼⠛⢦⣤⣄⡀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠢⡀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠲⠖⠛⠻⣿⡿⠛⠉⠉⠻⠷⣦⣽⠿⠿⠒⠚⠋⠉⠁⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⢀⣾⠛⠁⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠀⠀
+#⠀⠀⠀⠀⣰⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⠀⠀
+#⠀⠀⠀⣰⣿⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣧⣄⠀⠀⠀⠀⠀⠀⢳⡀⠀
+#⠀⠀⠀⣿⡾⢿⣀⢀⣀⣦⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⣫⣿⡿⠟⠻⠶⠀⠀⠀⠀⠀⢳⠀
+#⠀⠀⢀⣿⣧⡾⣿⣿⣿⣿⣿⡷⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⢀⡴⢿⣿⣧⠀⡀⠀⢀⣀⣀⢒⣤⣶⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+#⠀⠀⡾⠁⠙⣿⡈⠉⠙⣿⣿⣷⣬⡛⢿⣶⣶⣴⣶⣶⣶⣤⣤`⠤⠾⣿⣿⣿⡿⠿⣿⠿⢿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+#⠀⣸⠃⠀⠀⢸⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⠟⡉⠀⠀⠀⠈⠙⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+#⠀⣿⠀⠀⢀⡏⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀⠀⠉⠠⠿⠟⠻⠟⠋⠉⢿⣿⣦⡀⢰⡀⠀⠀⠀⠀⠀⠀⠁
+#⢀⣿⡆⢀⡾⠀⠀⠀⠀⣾⠏⢿⣿⣿⣿⣯⣙⢷⡄⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣻⢿⣷⣀⣷⣄⠀⠀⠀⠀⢸⠀
+#⢸⠃⠠⣼⠃⠀⠀⣠⣾⡟⠀⠈⢿⣿⡿⠿⣿⣿⡿⠿⠿⠿⠷⣄⠈⠿⠛⠻⠶⢶⣄⣀⣀⡠⠈⢛⡿⠃⠈⢿⣿⣿⡿⠀⠀⠀⠀⠀⡀
+#⠟⠀⠀⢻⣶⣶⣾⣿⡟⠁⠀⠀⢸⣿⢅⠀⠈⣿⡇⠀⠀⠀⠀⠀⣷⠂⠀⠀⠀⠀⠐⠋⠉⠉⠀⢸⠁⠀⠀⠀⢻⣿⠛⠀⠀⠀⠀⢀⠇
+#⠀⠀⠀⠀⠹⣿⣿⠋⠀⠀⠀⠀⢸⣧⠀⠰⡀⢸⣷⣤⣤⡄⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡆⠀⠀⠀⠀⡾⠀⠀⠀⠀⠀⠀⢼⡇
+#⠀⠀⠀⠀⠀⠙⢻⠄⠀⠀⠀⠀⣿⠉⠀⠀⠈⠓⢯⡉⠉⠉⢱⣶⠏⠙⠛⠚⠁⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⡇
+#⠀⠀⠀⠀⠀⠀⠻⠄⠀⠀⠀⢀⣿⠀⢠⡄⠀⠀⠀⣁⠁⡀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⢀⣐⡟⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⢠⡇
+#================= ================= =================
+# ================= GIT FUNCTIONS =================
+#================= ================= =================
+
+# GIT SYNCHRONIZE BRANCH
+# The function below manages pulling from master and rebasing onto the
+# current sub-branch you're on.
+function gitSyncBranch() {
+    local force=0
+    local branchName=$(git rev-parse --abbrev-ref HEAD)
+    local primaryBranch=$(git remote show origin | grep 'HEAD branch' | awk '{print $NF}')
+
+    # Check for -y flag to auto-confirm actions
+    if [[ " $@ " =~ " -y " ]]; then
+        force=1
+    fi
+
+    echo "Preparing to synchronize your branch: $branchName with the remote..."
+
+    # Fetch the latest changes from the remote repository
+    echo "Fetching the latest changes from the remote repository..."
+    git fetch origin
+
+    # Rebase current branch onto the primary branch
+    echo "Rebasing $branchName onto $primaryBranch..."
+    if git rebase origin/$primaryBranch; then
+        echo "Successfully rebased $branchName onto $primaryBranch."
+    else
+        echo "Rebase encountered conflicts. Please resolve conflicts and then continue the rebase."
+        return 1
+    fi
+
+    # Check for --no-push flag in the arguments
+    local no_push=0
+    for arg in "$@"; do
+        if [[ "$arg" == "--no-push" ]]; then
+            no_push=1
+            break
+        fi
+    done
+
+    # Push the changes upstream if --no-push flag wasn't set
+    if [ "$no_push" -eq 0 ]; then
+        echo "Pushing the changes upstream with --force-with-lease to ensure safety..."
+        if git push --force-with-lease; then
+            echo "Changes have been successfully pushed to $branchName."
+        else
+            echo "Failed to push changes. Please check for any issues and try again."
+            return 1
+        fi
+    else
+        echo "Skipping push operation as requested."
+    fi
+}
+
+alias sync-changes='gitSyncBranch'
+
+# GIT PUBLISH
+# automates the act of publishing a local branch from CLI
+function gitPublish() {
+    # Get current branch name
+    local branchName=$(git rev-parse --abbrev-ref HEAD)
+    echo "Preparing to publish your branch: $branchName"
+
+    # Check for unstaged changes
+    if [ -n "$(git status --porcelain)" ]; then
+        echo "Unstaged changes detected. Adding and committing..."
+        git add .
+
+        # Auto-generate a commit message or use a static one
+        # Example: "Auto-commit on publish"
+        git commit -m "Auto-commit on publish"
+    else
+        echo "No unstaged changes. Proceeding to publish..."
+    fi
+
+    # Push the current branch
+    git push -u origin $branchName
+    echo "Branch $branchName has been published."
+}
+# Configure simplified CLI call
+alias publish='gitPublish'
+
+# GIT SAVE CHANGES
+# semi-automates the act of staging your current changes and
+# You can provide your own committed message like git commit -m "",
+# or you can let vim let you edit your message.
+# ** Now you can set --no-push to keep the changes from pushing up on the branch.
+
+function gitSave() {
+    # Get current branch name
+    local branchName=$(git rev-parse --abbrev-ref HEAD)
+    echo "Checking for updates on the remote for branch: $branchName..."
+
+    # Pull the latest changes from the remote repository and rebase
+    git pull origin $branchName
+    if [ $? -ne 0 ]; then
+        echo "Error pulling changes from remote. Aborting 'gitSave'."
+        echo "Try running 'publish' first before 'save-changes'/resolve merge conflicts before saving again."
+        return 1
+    fi
+
+    # Check for --no-push flag in the arguments
+    local no_push=0
+    for input_string in "$@"; do
+        if [[ "$arg" == "--no-push" ]]; then
+            no_push=1
+            break
+        fi
+    done
+
+    # Stage current changes
+    git add .
+
+    # Check if a commit message was provided
+    if [ -z "$1" ]; then
+        # No commit message provided, fallback to default commit command
+        echo "No commit message provided. Opening default editor for commit message."
+        git commit
+    else
+        # Commit with the provided message
+        git commit -m "$1"
+    fi
+
+    # Push the changes upstream if --no-push flag wasn't set
+    if [ "$no_push" -eq 0 ]; then
+        echo "Pushing the changes upstream..."
+        git push
+        echo "Changes have been successfully pushed to $branchName."
+    else
+        echo "Skipping push operation as requested."
+    fi
+}
+alias save-changes='gitSave'
+
+# GIT DELETE REMOTE AND LOCAL BRANCH
+# This function automates the process of deleting the current branch from
+# both the remote repository and locally.
+# It also cleans up stale references to remote branches.
+function gitDeleteCurrentBranch() {
+    local force=0
+    local branchName=$(git rev-parse --abbrev-ref HEAD)
+    local primaryBranch=$(git remote show origin | grep 'HEAD branch' | awk '{print $NF}')
+
+    # Check for -y flag to auto-confirm actions
+    if [[ " $@ " =~ " -y " ]]; then
+        force=1
+    fi
+
+    # Fetch the latest changes and update primary branch
+    echo "Fetching the latest changes from the remote repository..."
+    git fetch origin
+    echo "Updating $primaryBranch with the latest changes..."
+    git switch "$primaryBranch"
+    git pull origin "$primaryBranch"
+
+    # Confirm deletion of the remote branch
+    if [ $force -eq 0 ]; then
+        echo -n "Are you sure you want to delete the remote branch '$branchName'? [y/N] "
+        read -r confirm
+        confirm=$(echo "$confirm" | tr '[:upper:]' '[:lower:]') # Convert to lowercase
+        if [[ "$confirm" != "y" ]]; then
+            echo "Deletion cancelled."
+            return
+        fi
+    else
+        echo "Auto-confirming deletion of the remote branch '$branchName'."
+    fi
+
+    # Delete the remote branch
+    git push origin --delete "$branchName"
+    if [ $? -ne 0 ]; then
+        echo "Failed to delete the remote branch '$branchName'. It may already be deleted."
+    fi
+
+    # Switch back to the main branch to delete the sub-branch
+    git switch "$primaryBranch"
+    
+    # Try to delete the local branch with -d first
+    git branch -d "$branchName"
+    if [ $? -ne 0 ]; then
+        # If -d fails, ask for confirmation to use -D
+        if [ $force -eq 0 ]; then
+            echo -n "The branch could not be deleted with -d. Use -D to force delete? [y/N] "
+            read -r confirmD
+            confirmD=$(echo "$confirmD" | tr '[:upper:]' '[:lower:]')
+            if [[ "$confirmD" != "y" ]]; then
+                echo "Force deletion cancelled."
+                return
+            fi
+        fi
+        git branch -D "$branchName" # Force delete the local branch
+    fi
+    
+    # Prune deleted remote references
+    git fetch --prune
+    echo "Cleanup complete. If you need to force actions next time, you can use the -y flag."
+}
+alias fully-delete-branch='gitDeleteCurrentBranch'
+# Usage:
+# - To interactively delete the current branch remotely and locally, cleaning up references:
+#   gitDeleteCurrentBranch
+# - To force deletion without confirmation:
+#   gitDeleteCurrentBranch -y
+
+#================= ================= =================
+# ================= BPS-FORMS-APP =================
+#================= ================= =================
+# The next line calls the bps-forms-app devtool utility script
+# EXAMPLE: devtool 3 --> This will call devtool and immediately input 3
+#  into the options for you.
+function callDevTool() {
+    echo "$1" | ~/bps/bps-forms-app/build/devtool.sh
+
+}
+alias devtool='callDevTool'
